cc1ff168b4c8a57615214c9ea189e04f0fef3a00
adsingh14 <deep.amansingh14@gmail.com>
2018-09-03 01:41:13 +0530

    Added card matching functionality

--- js/app.js
+++ js/app.js
@@ -4,29 +4,29 @@
  * Create a list that holds all of your cards
  */
 // Card deck
-const cards = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
-const allCards = cards.concat(cards);
+const cards = ["diamond", "paper-plane-o", "anchor", "bolt", "cube", "leaf", "bicycle", "bomb"];
+const cardList = cards.concat(cards);
 
 // shuffle list of cards
-shuffle(allCards);
+shuffle(cardList);
 
-// Place allCards in deck
+// Place cardList in deck
 const deck = document.querySelector('.deck');
 const temp_deck = document.createDocumentFragment();
 
-for (let card = 0; card < allCards.length; card++) {
-    let li = document.createElement('li');
-    li.classList = 'card';
+for (let card = 0; card < cardList.length; card++) {
 
-    let i = document.createElement('i');
-    i.classList = `fa ${allCards[card]}`;
-    li.appendChild(i);
+  let li = document.createElement('li');
+  li.className = 'card';
+  li.setAttribute('data-card', cardList[card]);
 
-    temp_deck.appendChild(li);
+  let i = document.createElement('i');
+  i.classList = `fa fa-${cardList[card]}`;
+
+  li.appendChild(i);
+  temp_deck.appendChild(li);
 }
 deck.appendChild(temp_deck);
-
-
 
 /*
  * Display the cards on the page
@@ -37,18 +37,18 @@ deck.appendChild(temp_deck);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
 /*
@@ -61,3 +61,43 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+const allCards = document.querySelectorAll('.card');
+let openCards = [];
+
+allCards.forEach(card => {
+  card.addEventListener('click', event => {
+    openCards.push(card);
+
+    // Disable event trigger on cards having 'open','show' and 'match' classes
+    if (!card.classList.contains('show') && !card.classList.contains('open') && !card.classList.contains('match')) {
+      card.classList.add('open', 'show');
+      if (openCards.length == 2) {
+        check();
+      }
+    }
+  });
+});
+
+// For checking the matched pair of cards
+const check = () => {
+  if (openCards[0].dataset.card == openCards[1].dataset.card) {
+
+    // Match
+    for (let card of openCards) {
+      card.classList.add('open', 'show', 'match');
+    }
+    openCards = [];
+  } else {
+
+    // Not matched
+    setTimeout(() => {
+      for (let card of openCards) {
+        card.classList.remove('open', 'show');
+      }
+      openCards = [];
+    }, 1500);
+  }
+};
+
+// TODO: Stop event on third click
